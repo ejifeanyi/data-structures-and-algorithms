0eced294aa8d8b50fcc468c4fde71ff73a09f476
Ifeanyi Emmanuel <ifeanyiemmanueljoseph@gmail.com>
2025-07-16 14:45:44 +0100

    solved: remove_duplicates

--- main.py
+++ main.py
@@ -47,7 +47,37 @@ print(max_num([1, -4, 100, -2])) # Output: 100
 # Double Every Number
 # Write a function that takes a list and returns a new list where every number is doubled.
 
+def doubled_list(numbers):
+    new_list = []
+    for num in numbers:
+        multiply = num * 2
+        new_list.append(multiply)
 
+    return new_list
+
+print(doubled_list([1, 2, 3, 4, 5])) # Output: [2, 4, 6, 8, 10]
 
 # Remove Duplicates
 # Write a function that removes duplicates from a list and returns the result.
+
+def remove_duplicates(numbers):
+    result = []
+    for num in numbers:
+        if num not in result:
+            result.append(num)
+
+    return result
+
+print(remove_duplicates([1, 1, 1, 3, 4, 6,])) # Output: [1, 3, 4, 6]
+
+def remove_duplicates_optimized(numbers):
+    seen = set()
+    result = []
+    for num in numbers:
+        if num not in seen:
+            seen.add(num)
+            result.append(num)
+
+    return result
+
+print(remove_duplicates_optimized([1, 1, 1, 3, 4, 6,])) # Output: [1, 3, 4, 6]
